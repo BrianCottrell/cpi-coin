92d32879037139a3b6c999c4e795dc01156343b6
Kaushik Donthi <kaushikdr@gmail.com>
2022-03-20 01:00:44 -0700

    updated pure token

--- packages/hardhat/contracts/PureToken.sol
+++ packages/hardhat/contracts/PureToken.sol
@@ -7,27 +7,37 @@ contract PureGovToken is  ERC20, ERC20Burnable {
   //We inherited the DetailedERC20
   uint256 public genesisTime;
 
-  constructor(string memory _name, string memory _symbol) ERC20("Pure Governance Token", "PGT") public {
+  constructor(string memory _name, string memory _symbol) ERC20("Pure Governance Token", "PGT") {
     genesisTime = block.timestamp;
   }
 
   function mintPure(address senderAddress, uint govTokenCount) public {
       // Convert governance tokens amount to dollasr
       uint conversionRate = getGovTokenPerDollar();
-      uint256 dollarsSpent = govTokenCount * conversionRate;
-      uint256 currTime = block.timestamp;
-      uint256 monthsSinceGenesis = (currTime - genesisTime) / (60 * 60 * 24 * 30);    
+      uint256 decs = decimals();
+      uint256 dollarsSpent= govTokenCount * conversionRate * 10^decs;
+      uint256 iud = inflationAdjustedDollars(dollarsSpent);
+      
 
-      uint256 inflationAdjustmentNumerator = 1005 ** monthsSinceGenesis;
-      uint256 inflationAdjustmentDenominator = 1000 ** monthsSinceGenesis;
-
-      uint256 inflationAdjustedDollars = dollarsSpent * inflationAdjustmentNumerator;
-      inflationAdjustedDollars = inflationAdjustedDollars / inflationAdjustmentDenominator;
-
-      _mint(senderAddress, inflationAdjustedDollars);
+      _mint(senderAddress, iud);
   }
 
-  function getGovTokenPerDollar() private returns (uint) {
+  function inflationAdjustedDollars(uint256 dollarsSpent) private returns (uint256) {
+    uint256 currTime = block.timestamp;
+    uint256 daysSinceGenesis = currTime - genesisTime;
+    daysSinceGenesis =  daysSinceGenesis / (60 * 60 * 24 * 30);    
+
+    uint256 inflationAdjustmentNumerator = 100004 ** daysSinceGenesis;
+    uint256 inflationAdjustmentDenominator = 100000 ** daysSinceGenesis;
+
+    uint256 inflationAdjustedDollars = dollarsSpent * inflationAdjustmentNumerator;
+    inflationAdjustedDollars = inflationAdjustedDollars / inflationAdjustmentDenominator;
+    return inflationAdjustedDollars;
+  }
+  
+        
+
+  function getGovTokenPerDollar() private pure returns (uint) {
       return 5; //TODO test
   }
 }
