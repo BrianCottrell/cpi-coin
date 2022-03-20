9847b58ea28f1c949273c19e3fb5fb0905bc7a20
Kabir Kapur <kapka6700@gmail.com>
2022-03-19 17:47:58 -0700

    adding half completed contracts

---
+++ packages/hardhat/contracts/cPG.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Permit.sol";
+
+contract cPGToken {
+    // beast
+}
